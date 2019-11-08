e56e0df9234f1f17653c27ca5e61080ef293d844
jiangzhao <1126571387@qq.com>
2019-11-08 10:26:55 +0800

    vue收集tops

--- src/pages/MSite/MSite.vue
+++ src/pages/MSite/MSite.vue
@@ -1,71 +1,35 @@
 <template>
       <section class="msite">
-       <HeaderTop title="昌平区北七家宏福科技园(337省道北)">
-          <span class="header_search" slot="left">
-            <i class="iconfont icon-sousuo"></i>
-          </span>
-          <span class="header_login" slot="right">
-            <span class="header_login_text">登录|注册</span>
-          </span>
+       <HeaderTop :title="address.name">
+      <router-link class="header_search" slot="left" to="/search">
+        <i class="iconfont icon-sousuo"></i>
+      </router-link>
+      <router-link class="header_login" slot="right" :to="userInfo._id ? '/userinfo': '/login'">
+        <span class="header_login_text" v-if="!userInfo._id">
+          登录|注册
+        </span>
+        <span class="header_login_text" v-else>
+           <i class="iconfont icon-person"></i>
+        </span>
+      </router-link>
        </HeaderTop>
         <!--首页导航-->
         <nav class="msite_nav">
-          <div class="swiper-container">
+          <div class="swiper-container" v-if="categorys.length">
             <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <a href="javascript:" class="link_to_food">
+             <div class="swiper-slide" v-for="(categorys, index) in categorysArr" :key="index">
+                <a href="javascript:" class="link_to_food" v-for="(category, index) in categorys" :key="index">
                   <div class="food_container">
-                    <img src="./images/nav/1.jpg">
+                      <img :src="baseImageUrl+category.image_url">
                   </div>
-                  <span>甜品饮品</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/2.jpg">
-                  </div>
-                  <span>商超便利</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/3.jpg">
-                  </div>
-                  <span>美食</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/4.jpg">
-                  </div>
-                  <span>简餐</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/5.jpg">
-                  </div>
-                  <span>新店特惠</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/6.jpg">
-                  </div>
-                  <span>准时达</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/7.jpg">
-                  </div>
-                  <span>预订早餐</span>
-                </a>
-                <a href="javascript:" class="link_to_food">
-                  <div class="food_container">
-                    <img src="./images/nav/8.jpg">
-                  </div>
-                  <span>土豪推荐</span>
+                  <span>{{category.title}}</span>
                 </a>
               </div>
             </div>
             <!-- Add Pagination -->
             <div class="swiper-pagination"></div>
           </div>
+           <img src="./images/msite_back.svg" alt="back" v-else>
         </nav>
         <!--首页附近商家-->
         <div class="msite_shop_list">
@@ -73,32 +37,99 @@
             <i class="iconfont icon-xuanxiang"></i>
             <span class="shop_header_title">附近商家</span>
           </div>
-        <ShooList></ShooList>
+        <ShopList></ShopList>
         </div>
       </section>
 </template>
 
 <script>
 import HeaderTop from '../../components/HeaderTop/HeaderTop'
-import ShooList from '../../components/ShopList/ShooList'
+import ShopList from '../../components/ShopList/ShopList'
 import Swiper from 'swiper'
 import 'swiper/css/swiper.min.css'
+import {mapState} from 'vuex'
 
 export default {
-    mounted() {
-      //创建Swiper对象实例，来轮播图
-      new Swiper ('.swiper-container',{
-        loop: true, // 循环模式选项
-        
-        // 如果需要分页器
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      })
+    data () {
+        return {
+        baseImageUrl: 'https://fuss10.elemecdn.com'
+      }
     },
+    mounted() {
+      //前台发送请求给后台，后台返回数据，可以在vue工具里面看。
+      //也相当于把数据存到vuex的state中的，因为vue工具看到的就是state的值
+      this.$store.dispatch('getCategorys')
+      this.$store.dispatch('getShops')
+    },
+    computed: {
+      //一般写在计算属性里面
+      //这里是取值，直接在state中取
+      ...mapState(['address','categorys', 'userInfo']),
+
+            /*
+      根据categorys一维数组生成一个2维数组
+      小数组中的元素个数最大是8
+       */
+      categorysArr () {
+        const {categorys} = this
+        // 准备空的2维数组
+        const arr = []
+        // 准备一个小数组(最大长度为8)
+        let minArr = []
+        // 遍历categorys
+        categorys.forEach(c => {
+          // 如果当前小数组已经满了, 创建一个新的
+          if(minArr.length===8) {
+            minArr = []
+          }
+          // 如果minArr是空的, 将小数组保存到大数组中
+          if(minArr.length===0) {
+            arr.push(minArr)
+          }
+          // 将当前分类保存到小数组中
+          minArr.push(c)
+        })
+
+        return arr
+      }
+    },
+
+    watch: {
+      categorys (value) { // categorys数组中有数据了, 在异步更新界面之前执行
+        // 使用setTimeout可以实现效果, 但不是太好
+        /*setTimeout(() => {
+          // 创建一个Swiper实例对象, 来实现轮播
+          new Swiper('.swiper-container', {
+            loop: true, // 可以循环轮播
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          })
+        }, 100)*/
+
+        // 界面更新就立即创建Swiper对象
+        this.$nextTick(() => {// 一旦完成界面更新, 立即调用(此条语句要写在数据更新之后)
+          // 创建一个Swiper实例对象, 来实现轮播
+          new Swiper('.swiper-container', {
+            loop: true, // 可以循环轮播
+            // 如果需要分页器
+            pagination: {
+              el: '.swiper-pagination',
+            },
+          })
+
+          // new BScroll('.miste-content-wrapper', {
+          //   click: true
+          // })
+        })
+
+      }
+    },
+
     components:{
       HeaderTop,
-      ShooList
+      ShopList
     }
 }
 </script>
